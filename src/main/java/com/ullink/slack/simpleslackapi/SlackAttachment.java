bf881bb4e64e55fc931ef2e01d7335f385939ba1
Amir Omidi <aa_omid@yahoo.com>
2015-07-13 04:56:22 +0430

    Added thumb_url

--- src/main/java/com/ullink/slack/simpleslackapi/SlackAttachment.java
+++ src/main/java/com/ullink/slack/simpleslackapi/SlackAttachment.java
@@ -3,23 +3,21 @@ package com.ullink.slack.simpleslackapi;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SlackAttachment
-{
-    public String           title;
-    public String           fallback;
-    public String           text;
-    public String           pretext;
+public class SlackAttachment {
+    public String title;
+    public String fallback;
+    public String text;
+    public String pretext;
+    public String thumb_url;
 
     public List<SlackField> fields;
 
-    public List<String>     markdown_in;
+    public List<String> markdown_in;
 
-    public SlackAttachment()
-    {
+    public SlackAttachment() {
     }
 
-    public SlackAttachment(String title, String fallback, String text, String pretext)
-    {
+    public SlackAttachment(String title, String fallback, String text, String pretext) {
         this.title = title;
         this.fallback = fallback;
         this.text = text;
@@ -27,24 +25,19 @@ public class SlackAttachment
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "SlackAttachment [title=" + title + ", fallback=" + fallback + ", text=" + text + ", pretext=" + pretext + ", fields=" + fields + "]";
     }
 
-    public void addField(String title, String value, boolean isShort)
-    {
-        if (fields == null)
-        {
+    public void addField(String title, String value, boolean isShort) {
+        if (fields == null) {
             fields = new ArrayList<>();
         }
         fields.add(new SlackField(title, value, isShort));
     }
 
-    public void addMarkdownIn(String value)
-    {
-        if (markdown_in == null)
-        {
+    public void addMarkdownIn(String value) {
+        if (markdown_in == null) {
             markdown_in = new ArrayList<>();
         }
         markdown_in.add(value);
--- src/main/java/com/ullink/slack/simpleslackapi/impl/SlackJSONAttachmentFormatter.java
+++ src/main/java/com/ullink/slack/simpleslackapi/impl/SlackJSONAttachmentFormatter.java
@@ -1,46 +1,41 @@
 package com.ullink.slack.simpleslackapi.impl;
 
+import com.ullink.slack.simpleslackapi.SlackAttachment;
+import com.ullink.slack.simpleslackapi.SlackField;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import com.ullink.slack.simpleslackapi.SlackAttachment;
-import com.ullink.slack.simpleslackapi.SlackField;
 
-class SlackJSONAttachmentFormatter
-{
-    public static List<JSONObject> encodeAttachments(SlackAttachment... attachments)
-    {
+class SlackJSONAttachmentFormatter {
+    public static List<JSONObject> encodeAttachments(SlackAttachment... attachments) {
         List<JSONObject> toReturn = new ArrayList<JSONObject>();
-        for (int i = 0; i < attachments.length; i++)
-        {
+        for (int i = 0; i < attachments.length; i++) {
             JSONObject attachmentJSON = new JSONObject();
             toReturn.add(attachmentJSON);
-            if (attachments[i].title != null)
-            {
+            if (attachments[i].title != null) {
                 attachmentJSON.put("title", attachments[i].title);
             }
-            if (attachments[i].text != null)
-            {
+            if (attachments[i].thumb_url != null) {
+                attachmentJSON.put("thumb_url", attachments[i].thumb_url);
+            }
+            if (attachments[i].text != null) {
                 attachmentJSON.put("text", attachments[i].text);
             }
-            if (attachments[i].pretext != null)
-            {
+            if (attachments[i].pretext != null) {
                 attachmentJSON.put("pretext", attachments[i].pretext);
             }
-            if (attachments[i].fallback != null)
-            {
+            if (attachments[i].fallback != null) {
                 attachmentJSON.put("fallback", attachments[i].fallback);
             }
-            if (attachments[i].markdown_in != null && !attachments[i].markdown_in.isEmpty())
-            {
+            if (attachments[i].markdown_in != null && !attachments[i].markdown_in.isEmpty()) {
                 JSONArray array = new JSONArray();
                 array.addAll(attachments[i].markdown_in);
                 attachmentJSON.put("mrkdwn_in", array);
             }
-            if (attachments[i].fields != null && !attachments[i].fields.isEmpty())
-            {
+            if (attachments[i].fields != null && !attachments[i].fields.isEmpty()) {
                 attachmentJSON.put("fields", encodeAttachmentFields(attachments[i].fields));
             }
 
@@ -48,19 +43,15 @@ class SlackJSONAttachmentFormatter
         return toReturn;
     }
 
-    private static List<JSONObject> encodeAttachmentFields(List<SlackField> fields)
-    {
+    private static List<JSONObject> encodeAttachmentFields(List<SlackField> fields) {
         List<JSONObject> toReturn = new ArrayList<JSONObject>();
-        for (SlackField field : fields)
-        {
+        for (SlackField field : fields) {
             JSONObject fieldJSON = new JSONObject();
             toReturn.add(fieldJSON);
-            if (field.title != null)
-            {
+            if (field.title != null) {
                 fieldJSON.put("title", field.title);
             }
-            if (field.value != null)
-            {
+            if (field.value != null) {
                 fieldJSON.put("value", field.value);
             }
             fieldJSON.put("short", field.isShort);
@@ -68,8 +59,7 @@ class SlackJSONAttachmentFormatter
         return toReturn;
     }
 
-    public static void main(String[] args) throws UnsupportedEncodingException
-    {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         System.out.println(new String("Loïc Herve".getBytes(), "UTF-8"));
     }
 }
