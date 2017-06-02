43070f8c4ff326bf093a7313377887ff641e8821
David Kowis <david@kow.is>
2017-06-01 23:43:09 -0500

    Add lombok, and put it on a class

--- sources/src/main/java/com/ullink/slack/simpleslackapi/events/PinAdded.java
+++ sources/src/main/java/com/ullink/slack/simpleslackapi/events/PinAdded.java
@@ -3,7 +3,9 @@ package com.ullink.slack.simpleslackapi.events;
 import com.ullink.slack.simpleslackapi.SlackFile;
 import com.ullink.slack.simpleslackapi.SlackUser;
 import com.ullink.slack.simpleslackapi.SlackChannel;
+import lombok.Data;
 
+@Data
 public class PinAdded implements SlackEvent {
 
   private final SlackUser sender;
@@ -12,34 +14,6 @@ public class PinAdded implements SlackEvent {
   private final SlackFile file;
   private final String message;
 
-  public PinAdded(SlackUser sender, SlackChannel channel, String timestamp, SlackFile file, String message) {
-        this.sender = sender;
-        this.channel = channel;
-        this.timestamp = timestamp;
-        this.file = file;
-        this.message = message;
-    }
-
-    public SlackUser getSender() {
-        return this.sender;
-    }
-
-    public SlackChannel getChannel() {
-        return this.channel;
-    }
-
-    public String getTimestamp() {
-        return this.timestamp;
-    }
-
-    public SlackFile getFile() {
-        return this.file;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-  
     @Override
     public SlackEventType getEventType() {
         return SlackEventType.PIN_ADDED;
