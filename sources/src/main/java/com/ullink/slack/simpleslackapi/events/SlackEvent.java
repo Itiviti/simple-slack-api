5012228ec83855c65b8290bad8d383d56859d5aa
Kahlil Oppenheimer <koppenheimer@hubspot.com>
2017-12-05 14:50:17 +0000

    Replaced usages of SlackEvent.UNKNOWN_EVENT with UnkownEvent
    A ClassCastException was being caused by SlackEvent.UNKOWN_EVENT being
    passed around, where later, some logic would try to cast it into an
    UnknownEvent. Now, SlackEvent.UNKNOWN_EVENT is removed and completely
    replaced by UnknownEvent, which should prevent this exception

--- sources/src/main/java/com/ullink/slack/simpleslackapi/events/SlackEvent.java
+++ sources/src/main/java/com/ullink/slack/simpleslackapi/events/SlackEvent.java
@@ -2,15 +2,6 @@ package com.ullink.slack.simpleslackapi.events;
 
 public interface SlackEvent {
 
-    SlackEvent UNKNOWN_EVENT = new SlackEvent()
-    {
-
-        @Override
-        public SlackEventType getEventType()
-        {
-            return SlackEventType.UNKNOWN;
-        }
-    };
-    
     SlackEventType getEventType();
+
 }
