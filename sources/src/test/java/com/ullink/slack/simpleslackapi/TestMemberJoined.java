c287421103ed7c0c1c2acdc71541eeedb21b35fd
rajanpatel4578 <33018692+rajanpatel4578@users.noreply.github.com>
2021-11-17 12:34:56 -0800

    Add files via upload
    Sample code for Member Joined Channel Listener implementation.

---
+++ sources/src/test/java/com/ullink/slack/simpleslackapi/TestMemberJoined.java
@@ -0,0 +1,34 @@
+import com.ullink.slack.simpleslackapi.SlackChannel;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.SlackUser;
+import com.ullink.slack.simpleslackapi.events.*;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import com.ullink.slack.simpleslackapi.listeners.SlackChannelJoinedListener;
+import com.ullink.slack.simpleslackapi.listeners.SlackChannelLeftListener;
+import com.ullink.slack.simpleslackapi.listeners.SlackMemberJoinedListener;
+import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+
+import java.io.IOException;
+
+public class TestMemberJoined {
+    public static void main(String[] args) throws IOException {
+        SlackSession session = SlackSessionFactory.createWebSocketSlackSession(" xoxb..... - add bot auth token here");
+        session.connect();
+        SlackChannel channel = session.findChannelByName("general"); //make sure bot is a member of the channel.
+        session.sendMessage(channel, "hello world, i'm a bot and I just connected" );
+
+        // Define the Member Joined listener
+		SlackMemberJoinedListener slackMemberJoinedListener = new SlackMemberJoinedListener() {
+           	public void onEvent(SlackMemberJoined event, SlackSession session) {
+				SlackChannel slackChannel = event.getChannel();
+                if(event.getChannel()==null)
+                    System.out.println("Error: Getting slack channel as null");
+                session.sendMessage(event.getChannel(),"Welcome to the channel :) !");
+				System.out.println(event.toString());
+			}
+		};
+
+        //add it to the session
+        session.addSlackMemberJoinedListener(slackMemberJoinedListener);
+    }
+}
