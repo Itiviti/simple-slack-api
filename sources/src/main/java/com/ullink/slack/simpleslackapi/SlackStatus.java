5851ab17dd34710de942d698050eb30140c9e94b
Sandeep Yerramsetty <sandeep-yerramsetty@mercari.com>
2021-11-21 14:39:23 -0800

    add comment

--- sources/src/main/java/com/ullink/slack/simpleslackapi/SlackStatus.java
+++ sources/src/main/java/com/ullink/slack/simpleslackapi/SlackStatus.java
@@ -8,23 +8,45 @@ public class SlackStatus {
     @SerializedName("emoji")
     private String emoji;
 
+    /**
+     * Sets the slack status text of a user profile
+     *
+     * @param txt is the text of the slack status
+     * @return SlackStatus
+     */
     public SlackStatus setText(String txt)
     {
         text = txt;
         return this;
     }
 
+    /**
+     * Sets the slack status emoji of a user profile
+     *
+     * @param emoji is the emoji of the slack status
+     * @return SlackStatus
+     */
     public SlackStatus setEmoji(String emoji)
     {
         this.emoji = emoji;
         return this;
     }
 
+    /**
+     * Returns the slack status text of a user profile
+     *
+     * @return void
+     */
     public String getText()
     {
         return text;
     }
 
+    /**
+     * Returns the slack status emoji of a user profile
+     *
+     * @return void
+     */
     public String getEmoji()
     {
         return emoji;
